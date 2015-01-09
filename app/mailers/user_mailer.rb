3047a84fe66b1a1e89ac1f92fc9d3dbbbdfa96d8
josh <josh@mojotech.com>
2015-01-09 13:49:02 -0500

    Add Forgot password email feature

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ActionMailer::Base
     @site_name = "localhost"
     mail(:to => user.email, :subject => 'Welcome to my website.')
   end
+
+  def reset_password_email(user)
+    @user = user
+    @password_reset_url = 'http://localhost:3000/password_reset?' + @user.password_reset_token
+    mail(:to => user.email, :subject => 'Password Reset Instructions.')
+  end
 end
